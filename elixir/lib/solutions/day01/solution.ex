b782a2757e005a2d42356d3ac26b26a4c7b3c23e
Ethan Niser <100045248+ethanniser@users.noreply.github.com>
2023-12-01 17:56:03 +0000

    test passes but no cigar :(

--- elixir/lib/solutions/day01/solution.ex
+++ elixir/lib/solutions/day01/solution.ex
@@ -1,12 +1,90 @@
 defmodule Day01.Part1 do
   def solve(input) do
-    0
+    input
+    |> String.split("\n")
+    |> Enum.map(fn line ->
+      line
+      |> String.split("")
+      |> Enum.filter(&String.match?(&1, ~r/\d/))
+    end)
+    |> Enum.map(fn
+      [head] -> {head, head}
+      [head | rest] -> {head, List.last(rest)}
+    end)
+    |> Enum.map(fn {first, second} ->
+      String.to_integer(first <> second)
+    end)
+    |> Enum.sum()
   end
 end
 
 defmodule Day01.Part2 do
   def solve(input) do
-    0
+    input
+    |> String.split("\n")
+    |> Enum.map(&parse_digits(&1, []))
+    |> Enum.map(fn
+      [head] -> {head, head}
+      [head | rest] -> {head, List.last(rest)}
+    end)
+    |> Enum.map(fn {first, second} ->
+      String.to_integer(first <> second)
+    end)
+    |> Enum.sum()
+  end
+
+  def parse_string(str) do
+    case str do
+      "one" <> rest ->
+        {"1", rest}
+
+      "two" <> rest ->
+        {"2", rest}
+
+      "three" <> rest ->
+        {"3", rest}
+
+      "four" <> rest ->
+        {"4", rest}
+
+      "five" <> rest ->
+        {"5", rest}
+
+      "six" <> rest ->
+        {"6", rest}
+
+      "seven" <> rest ->
+        {"7", rest}
+
+      "eight" <> rest ->
+        {"8", rest}
+
+      "nine" <> rest ->
+        {"9", rest}
+
+      "" ->
+        nil
+
+      other ->
+        first_char = String.slice(other, 0, 1)
+        rest = String.slice(other, 1, String.length(other))
+
+        if String.match?(first_char, ~r/\d/) do
+          {first_char, rest}
+        else
+          {nil, rest}
+        end
+    end
+  end
+
+  defp parse_digits("", acc), do: Enum.reverse(acc)
+
+  defp parse_digits(str, acc) do
+    case parse_string(str) do
+      {nil, rest} -> parse_digits(rest, acc)
+      {digit, rest} -> parse_digits(rest, [digit | acc])
+      nil -> Enum.reverse(acc)
+    end
   end
 end
 
--- elixir/test/day01_test.exs
+++ elixir/test/day01_test.exs
@@ -2,15 +2,29 @@ defmodule Day01Test do
   use ExUnit.Case
 
   @example_input """
-
+                 1abc2
+                 pqr3stu8vwx
+                 a1b2c3d4e5f
+                 treb7uchet
                  """
                  |> Utils.normalize_input()
 
+  @part2_example_input """
+                       two1nine
+                       eightwothree
+                       abcone2threexyz
+                       xtwone3four
+                       4nineeightseven2
+                       zoneight234
+                       7pqrstsixteen
+                       """
+                       |> Utils.normalize_input()
+
   test "solves example input for part 1" do
-    assert Day01.Part1.solve(@example_input) == 0
+    assert Day01.Part1.solve(@example_input) == 142
   end
 
   test "solves example input for part 2" do
-    assert Day01.Part2.solve(@example_input) == 0
+    assert Day01.Part2.solve(@part2_example_input) == 281
   end
 end
