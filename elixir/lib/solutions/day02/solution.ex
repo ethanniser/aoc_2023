e0a37ca4bba688b6381496df9e27666ebe6a3eba
Ethan Niser <thebestgamer2502@gmail.com>
2023-12-02 11:31:44 -0600

    day 2 part 1

---
+++ elixir/lib/solutions/day02/solution.ex
@@ -0,0 +1,78 @@
+defmodule Day02.Part1 do
+  @max_cubes %{
+    red: 12,
+    green: 13,
+    blue: 14
+  }
+
+  @default_cubes %{
+    red: 0,
+    green: 0,
+    blue: 0
+  }
+
+  def solve(input) do
+    input
+    |> String.split("\n")
+    |> Enum.map(fn line ->
+      line
+      |> String.split(": ")
+      |> Enum.at(1)
+      |> String.split("; ")
+      |> Enum.map(fn reveal ->
+        reveal
+        |> String.split(", ")
+        |> Enum.reduce(@default_cubes, fn num_and_color, acc ->
+          cond do
+            String.ends_with?(num_and_color, " blue") ->
+              num = String.slice(num_and_color, 0..-6)
+              %{acc | blue: String.to_integer(num)}
+
+            String.ends_with?(num_and_color, " red") ->
+              num = String.slice(num_and_color, 0..-5)
+              %{acc | red: String.to_integer(num)}
+
+            String.ends_with?(num_and_color, " green") ->
+              num = String.slice(num_and_color, 0..-7)
+              %{acc | green: String.to_integer(num)}
+          end
+        end)
+      end)
+      |> Enum.reduce(@default_cubes, fn reveal_map, acc ->
+        %{
+          blue: max(reveal_map.blue, acc.blue),
+          red: max(reveal_map.red, acc.red),
+          green: max(reveal_map.green, acc.green)
+        }
+      end)
+    end)
+    |> Enum.with_index(1)
+    |> Enum.filter(fn {game, _} ->
+      not (game.blue > @max_cubes.blue or game.red > @max_cubes.red or
+             game.green > @max_cubes.green)
+    end)
+    |> Enum.map(&elem(&1, 1))
+    |> Enum.sum()
+  end
+end
+
+defmodule Day02.Part2 do
+  def solve(input) do
+    0
+  end
+end
+
+defmodule Mix.Tasks.Day02 do
+  use Mix.Task
+
+  def run(_) do
+    {:ok, raw_input} = File.read("lib/solutions/day02/input.txt")
+    input = raw_input |> Utils.normalize_input()
+
+    IO.puts("--- Part 1 ---")
+    IO.puts(Day02.Part1.solve(input))
+    IO.puts("")
+    IO.puts("--- Part 2 ---")
+    IO.puts(Day02.Part2.solve(input))
+  end
+end
---
+++ elixir/test/day02_test.exs
@@ -0,0 +1,20 @@
+defmodule Day02Test do
+  use ExUnit.Case
+
+  @example_input """
+                 Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+                 Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+                 Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+                 Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+                 Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+                 """
+                 |> Utils.normalize_input()
+
+  test "solves example input for part 1" do
+    assert Day02.Part1.solve(@example_input) == 8
+  end
+
+  test "solves example input for part 2" do
+    assert Day02.Part2.solve(@example_input) == 0
+  end
+end
